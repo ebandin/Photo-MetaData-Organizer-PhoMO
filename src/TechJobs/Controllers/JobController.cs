001694bd8e10b2a5412efef2a653f021c2d387bf
Eric Bandin <ericmbandin@gmail.com>
2019-12-31 14:30:58 -0800

    Add project files.

---
+++ src/TechJobs/Controllers/JobController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechJobs.Data;
+using TechJobs.ViewModels;
+using System;
+using System.Collections.Generic;
+using TechJobs.Models;
+
+namespace TechJobs.Controllers
+{
+    public class JobController : Controller
+    {
+
+        // Our reference to the data store
+        private static JobData jobData;
+
+        static JobController()
+        {
+            jobData = JobData.GetInstance();
+        }
+
+        public List<Job> Jobs { get; private set; }
+// TODO # 1
+        // The detail display for a given Job at URLs like /Job?id=17
+        public IActionResult Index(int id)
+        {
+            
+            Jobs = jobData.FindByValue(id);
+
+            return View();
+        }
+
+        public IActionResult New()
+        {
+            NewJobViewModel newJobViewModel = new NewJobViewModel();
+            return View(newJobViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult New(NewJobViewModel newJobViewModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job
+                {
+                    Name = newJobViewModel.Name,
+                    //Employer = JobData.Find(newJobViewModel.EmployerID),
+                    Employer = newJobViewModel.EmployerID,
+                    Location = newJobViewModel.Location,
+                    CoreCompetency =
+                    PositionType =
+
+                    //newJobViewModel.
+                };
+                // th jobcontroller connects to the JobData function of Find
+                //  change how the DataImporter works with the Job fields
+                // 
+                return View("Index", newJobViewModel);
+
+            }
+            else
+            {
+                newJobViewModel.Jobs = jobData.FindByColumnAndValue(newJobViewModel.Column, newJobViewModel.Value);
+            }
+
+            newJobViewModel.Title = "Search";
+
+            ;
+
+            //TODO #6 - Validate the ViewModel and if valid, create a 
+            //new Job and add it to the JobData data store.Then
+            //redirect to the Job detail(Index) action/view for the new Job.
+
+            //return View("Index");
+            return View(newJobViewModel);
+        }
+    }
+}
---
+++ src/TechJobs/ViewModels/NewJobViewModel.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using TechJobs.Data;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+    public class NewJobViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Employer")]
+        public int EmployerID { get; set; }
+
+        [Required]
+        [Display(Name = "Location")]
+        public int LocationsID { get; set; }
+
+        [Required]
+        [Display(Name = "Position")]
+        public int PositionTypesID { get; set; }
+
+        [Required]
+        [Display(Name = "CoreCompetency")]
+        public int CoreCompetenciesID { get; set; }
+
+
+
+
+
+        // TODO #3 - Included other fields needed to create a job,
+        // with correct validation attributes and display names.
+        //take the example here and make it each one, rather than only apply to one
+        public List<SelectListItem> Employers { get; set; } = new List<SelectListItem>();
+        public List<SelectListItem> Locations { get; set; } = new List<SelectListItem>();
+        public List<SelectListItem> CoreCompetencies { get; set; } = new List<SelectListItem>();
+        public List<SelectListItem> PositionTypes { get; set; } = new List<SelectListItem>();
+
+        public NewJobViewModel()
+        {
+
+            JobData jobData = JobData.GetInstance();
+
+            foreach (Employer field in jobData.Employers.ToList())
+            {
+                Employers.Add(new SelectListItem {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+            foreach (Location field in jobData.Locations.ToList())
+            {
+                Locations.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+            foreach (CoreCompetency field in jobData.CoreCompetencies.ToList())
+            {
+                CoreCompetencies.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+            foreach (PositionType field in jobData.PositionTypes.ToList())
+            {
+                PositionTypes.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+
+            // Populate the list of all columns
+
+
+            // TODO #4 - populate the other List<SelectListItem> 
+            // collections needed in the view
+
+        }
+    }
+}
